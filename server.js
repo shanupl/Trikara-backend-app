878b5c9f96c480d3242bee15b8048a340ee42d05
Shanu Panchal <ranupl542011@gmail.com>
2024-09-05 15:34:05 +0530

    api creation for workflow

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path")
 const userRoutesV1 = require("./src/routes/v1/user.route")
 const { connectDB } = require('./src/config/db');
 const PORT = process.env.PORT || PORT;
+const axios = require("axios");
 const cors = require("cors");
 
 connectDB();
@@ -13,6 +14,28 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/users/', userRoutesV1);
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const REPO_OWNER = 'shanupl';
+const REPO_NAME = 'Trikara-backend-app';
+
+// Route to fetch GitHub Actions workflow runs
+app.get("/api/workflows", async (req, res) => {
+    try {
+        const response = await axios.get(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs`, {
+            headers: {
+                Authorization: `Bearer ${GITHUB_TOKEN}`,
+                Accept: 'application/vnd.github.v3+json'
+            }
+        });
+        const workflows = response.data;
+        console.log(workflows, "workflows---")
+        res.json(workflows); // Send the workflow data to the frontend
+    } catch (error) {
+        console.error("Error fetching workflow runs:", error);
+        res.status(500).json({ message: "Failed to fetch workflow runs" });
+    }
+});
 
 app.get("/", (req, res) => {
     return res.json("App is running----")
